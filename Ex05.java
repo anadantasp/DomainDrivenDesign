c4ae27088e409779865c793697913d0ba34ea102
anacarolinaprado <111712865+anacarolinaprado@users.noreply.github.com>
2022-08-23 22:54:45 -0300

    Adicionando exercícios dia 23/08

---
+++ Ex05.java
@@ -0,0 +1,22 @@
+package entrada_processamento_saida;
+
+import java.util.Scanner;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		double tempC, tempF;
+		
+		System.out.printf("Digite a temperatura em Celsius: ");
+		tempC = scn.nextFloat();
+		
+		tempF = (tempC * 1.8) + 32;
+		
+		System.out.printf("Temperatura em fahrenheit: %.1f", tempF);
+		
+		
+	}
+
+}
