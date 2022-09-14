0bc112e5d3182677ba0f3581ccbf6e52c24927dc
anadantasp <caroldantasp@gmail.com>
2022-09-13 22:25:06 -0300

    Adicionando os 3 últimos exercícios adicionados na lista

---
+++ src/entrada_processamento_saida/Ex48.java
@@ -0,0 +1,25 @@
+package entrada_processamento_saida;
+
+import java.util.Scanner;
+
+public class Ex48 {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.printf("Digite um número: ");
+		num = scn.nextInt();
+		
+		for(int i = 1; i <= 20; i++) {
+			num = num + 1;
+			System.out.print(num + ", ");
+		}
+		
+		
+
+	}
+
+}
